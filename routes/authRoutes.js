3d4b318eda2553ec3c05af4816fadc623586dedc
Sherry <sm718036@gmail.com>
2025-03-26 12:22:34 +0500

    fix bugs as code which is not used is removed

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -18,7 +18,7 @@ router.post("/register", async (req, res) => {
     const existingEmail = await collection.findOne({ email: req.body.email });
 
     if (existingEmail) {
-      logger.error("Email already exists");
+      console.error("Email already exists");
       return res.status(400).json({ error: "Email id already exists" });
     }
 
@@ -41,10 +41,10 @@ router.post("/register", async (req, res) => {
     };
 
     const authtoken = jwt.sign(payload, JWT_SECRET);
-    logger.info("User registered successfully");
+    console.log("User registered successfully");
     res.json({ authtoken, email });
   } catch (e) {
-    logger.error(e);
+    console.error(e);
     return res.status(500).send("Internal server error");
   }
 });
@@ -61,7 +61,7 @@ router.post("/login", async (req, res) => {
     if (theUser) {
       let result = await bcryptjs.compare(req.body.password, theUser.password);
       if (!result) {
-        logger.error("Passwords do not match");
+        console.error("Passwords do not match");
         return res.status(404).json({ error: "Wrong pasword" });
       }
       let payload = {
@@ -74,14 +74,14 @@ router.post("/login", async (req, res) => {
       const userEmail = theUser.email;
 
       const authtoken = jwt.sign(payload, JWT_SECRET);
-      logger.info("User logged in successfully");
+      console.log("User logged in successfully");
       return res.status(200).json({ authtoken, userName, userEmail });
     } else {
-      logger.error("User not found");
+      console.error("User not found");
       return res.status(404).json({ error: "User not found" });
     }
   } catch (e) {
-    logger.error(e);
+    console.error(e);
     return res
       .status(500)
       .json({ error: "Internal server error", details: e.message });
@@ -96,7 +96,7 @@ router.put("/update", async (req, res) => {
 
   // Task 3: Check if `email` is present in the header and throw an appropriate error message if not present.
   if (!errors.isEmpty()) {
-    logger.error("Validation errors in update request", errors.array());
+    console.error("Validation errors in update request", errors.array());
     return res.status(400).json({ errors: errors.array() });
   }
 
@@ -104,7 +104,7 @@ router.put("/update", async (req, res) => {
     const email = req.headers.email;
 
     if (!email) {
-      logger.error("Email not found in the request headers");
+      console.error("Email not found in the request headers");
       return res
         .status(400)
         .json({ error: "Email not found in the request headers" });
@@ -118,7 +118,7 @@ router.put("/update", async (req, res) => {
     const existingUser = await collection.findOne({ email });
 
     if (!existingUser) {
-      logger.error("User not found");
+      console.error("User not found");
       return res.status(404).json({ error: "User not found" });
     }
 
@@ -140,11 +140,11 @@ router.put("/update", async (req, res) => {
     };
 
     const authtoken = jwt.sign(payload, JWT_SECRET);
-    logger.info("User updated successfully");
+    console.log("User updated successfully");
 
     res.json({ authtoken });
   } catch (error) {
-    logger.error(error);
+    console.error(error);
     return res.status(500).send("Internal Server Error");
   }
 });
